cf225585821ff52aecc5437031ff8381a715d1ef
ndundastevn <122885990+ndundastevn@users.noreply.github.com>
2023-04-28 12:24:55 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -13,7 +13,17 @@
 #include <signal.h> /* for signal management */
 #include <fcntl.h> /* for open files*/
 
-/************* MACROS **************/
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+
+/********* MACROS **************/
 
 #include "macros.h" /* for msg help and prompt */
 
