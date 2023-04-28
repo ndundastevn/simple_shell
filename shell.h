8cabb7acb347f9c20e9af59f8148d2735239971b
ndundastevn <122885990+ndundastevn@users.noreply.github.com>
2023-04-28 12:42:14 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,17 +1,40 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h> /* for printf*/
-#include <unistd.h> /* for fork, execve*/
-#include <stdlib.h>
-#include <string.h> /* for strtok*/
-#include <stddef.h>
 #include <errno.h> /* for errno and perror */
 #include <sys/types.h> /* for type pid */
 #include <sys/wait.h> /* for wait */
 #include <sys/stat.h> /* for use of stat function */
 #include <signal.h> /* for signal management */
 #include <fcntl.h> /* for open files*/
+#include <stdio.h> /* for printf*/
+#include <unistd.h> /* for fork, execve*/
+#include <stdlib.h>
+#include <string.h> /* for strtok*/
+#include <stddef.h>
+
+
+/*************Minecrones************/
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 /************* MACROS **************/
 
