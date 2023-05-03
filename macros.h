960e5b619c4309abccdb9099dfea0d11378406cf
ndundastevn <122885990+ndundastevn@users.noreply.github.com>
2023-05-03 11:07:27 +0300

    simple shell project

---
+++ macros.h
@@ -0,0 +1,82 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+/*********mincrones********/
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+/* Prompt to be printed */
+#define PROMPT_MSG "$" /* Needed to work with signal */
+
+/* Resume from the unused attibute */
+#define UNUSED __attribute__((unused))
+
+/* buffer size for each read call in _getline */
+#define BUFFER_SIZE 1024
+
+/************* FORMATTED STRING FOR HELP BUILT IN **************/
+
+#define HELP_CD_MSG "cd=\n"\
+"cd:\tcd [dir]\n\n"\
+"	Change the shell working directory.\n\n"\
+"	If no argument is given to cd the command will be interpreted\n"\
+"	as cd $HOME.\n"\
+"	if the argumenthelp is - (cd -), the command will be interpreted\n"\
+"	as cd $OLDPWD.\n\n"
+
+#define HELP_EXIT_MSG "exit=\n"\
+"exit:\texit [STATUS]\n\n"\
+"	Exit of the simple-shell.\n\n"\
+"	Exits the shell with a status of N.  If N is omitted, the exit status\n"\
+"	is that of the last command executed.\n\n"
+
+#define HELP_ENV_MSG "env=\n"\
+"env:\tenv \n\n"\
+"	Print environment.\n\n"\
+"	The env command will be print a complete list of enviroment variables.\n\n"
+
+#define HELP_SETENV_MSG "setenv=\n"\
+"setenv:\tsetenv VARIABLE VALUE\n\n"\
+"	Change or add an environment variable.\n\n"\
+"	initialize a new environment variable, or modify an existing one\n"\
+"	When there are not correct numbers of arguments print error message.\n\n"
+
+#define HELP_UNSETENV_MSG "unsetenv=\n"\
+"unsetenv:\tunsetenv VARIABLE\n\n"\
+"	The unsetenv function deletes one variable from the environment.\n\n"\
+"	Wen there are not correct numbers of arguments print error message.\n\n"
+
+#define HELP_MSG "help=\n"\
+"help:\thelp [BUILTIN_NAME]\n\n"\
+"	Display information about builtin commands.\n\n"\
+"	Displays brief summaries of builtin commands.  If BUILTIN_NAME is\n"\
+"	specified, gives detailed help on all commands matching BUILTIN_NAME,\n"\
+"	otherwise the list of help topics is printed BUILTIN_NAME list.\n"\
+"	Arguments:\n\n"\
+"	BUILTIN_NAME specifiying a help topic.\n\n"\
+"	cd\t[dir]\n"\
+"	exit\t[status]\n"\
+"	env\n"\
+"	setenv\t[variable value]\n"\
+"	unset\t[variable]\n"\
+"	help\t[built_name]\n\n"
+
+#endif
